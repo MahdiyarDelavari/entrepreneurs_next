4fab2f31221410f2a88c013c8f64375a3f0d452e
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2025-06-27 11:55:29 +0330

    build search bar component and updating the hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,21 @@
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{ query?: string }>
+}) {
+
+  const query = (await searchParams).query
   return (
     <>
-      <section className="pink_container">
+      <section className="blue_container">
 
         <h1 className="heading">Pitch Uour Startup, <br />Connect With Entrupenures</h1>
-        
+
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote On Pitches, And Get Noticed In virtual
+          Submit Ideas, Vote On Pitches, And Get Noticed In Virtual.
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
